1340e6962ad0064636cc4be5fcf76fed1f8a9ae1
Murali981 <muraliju981@gmail.com>
2024-09-17 15:17:23 +0530

    Sample testing of event emitters is done

--- events.js
+++ events.js
@@ -1,6 +1,6 @@
 const EventEmitter = require("events"); // events is a built-in module available in node.js
 
-const myEmitter = new EventEmitter(); // We are creating a instance of EventEmitter class
+const http = require("http"); // http is a built-in module available in node.js
 
 /* The above EventEmitters can emit named events and we can subscribe to these events. So basically listen to them  and then 
  react accordingly */
@@ -10,6 +10,8 @@ class Sales extends EventEmitter {
     super();
   }
 }
+
+const myEmitter = new Sales(); // We are creating a instance of Sales class
 
 myEmitter.on("newSale", () => {
   console.log("There was a new sale!");
@@ -25,3 +27,25 @@ myEmitter.on("newSale", (stock) => {
 // 9 as an argument to this callback function
 
 myEmitter.emit("newSale", 9); // We have emitted this newSale event with stock of 9
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request received!");
+  console.log(req.url);
+  res.end("Request received");
+});
+
+server.on("request", (req, res) => {
+  console.log("Another Request received");
+});
+
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});
